5db164cc773abf614f1e4e1996c016721fb2b15d
Luan Oliveira <luanconecte@gmail.com>
2016-08-19 22:30:02 -0300

    atualização do container

--- lamp.sh
+++ lamp.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-if [ $(docker ps -f name=apache-php -q) ]; then
-    docker kill apache-php
+if ( docker ps -a -f name=mysql -q ); then
+   docker start mysql \
+      && docker exec mysql /etc/init.d/mysql start
+else
+   docker run -di --name=mysql -p 3306:3306 luanoliveira/mysql:1.0 \
+      && docker exec mysql /etc/init.d/mysql start
 fi
 
-if [ $(docker ps -a -f name=apache-php -q) ]; then
-    docker rm apache-php
+if ( docker ps -a -f name=apache-php -q ); then
+   docker start apache-php \
+      && docker exec apache-php /etc/init.d/apache2 start
+else
+   docker run -di --name=apache-php -p 8000:80 -v ~/www:/var/www/html --link=mysql:mysql luanoliveira/apache-php:1.0 \
+      && docker exec apache-php /etc/init.d/apache2 start
 fi
-
-if [ $(docker ps -f name=mysql -q) ]; then
-    docker kill mysql
-fi
-
-if [ $(docker ps -a -f name=mysql -q) ]; then
-    docker rm mysql
-fi
-
-docker run -di --name=apache-php -v ~/www:/var/www/html luanoliveira/apache-php:1.0
-
-docker exec apache-php /etc/init.d/apache2 start
-
-docker run -di --name=mysql -p 3306:3306 luanoliveira/mysql:1.0
-
-docker exec mysql /etc/init.d/mysql start
-
-echo "FLAMENGO SEMPRE!"
